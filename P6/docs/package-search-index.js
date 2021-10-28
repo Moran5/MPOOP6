fc99d966f7c165f67188a160ba7907cec431fc25
Moran5 <93295627+Moran5@users.noreply.github.com>
2021-10-27 19:26:47 -0500

    Delete P6 directory

--- P6/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"P6.MPOOP6"}]
--- P6/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"P6.MPOOP6","l":"main"}]
