e70a2df4f0dc17b4beac59c17b0995127ad1a412
Moran5 <93295627+Moran5@users.noreply.github.com>
2021-10-27 19:27:46 -0500

    Add files via upload

---
+++ MPOOP6/main.java
@@ -0,0 +1,17 @@
+package MPOOP6;
+/**
+ *@author Moran Duque, Jose Alejandro
+ */
+public class main{
+  public static void main(String[] args) {
+    System.out.println("La suma es:"+Suma(2,2));
+  }
+  /**
+  *@param a Primer entero de la suma
+  *@param b Segundo entero de la suma
+  *@return La suma de los enteros a y b
+  */
+  public static int Suma(int a, int b){
+      return a+b;
+    }
+}
